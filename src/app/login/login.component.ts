18a65e81a92b4eb8e2b6a9ebdbc64fdb5bc5b9be
Juan Guadalupe <jaguadal@espol.edu.ec>
2023-10-19 23:47:04 -0500

    ESLINT installation and checking

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,23 @@
-import { Component,Output,EventEmitter } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Output, EventEmitter } from '@angular/core'
+import { type Router } from '@angular/router'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private router:Router){}
-  username: string = '';
-  password: string = '';
-  showPassword: boolean = false;
+  constructor (private readonly router: Router) {}
+  username: string = ''
+  password: string = ''
+  showPassword: boolean = false
   @Output() onLogin: EventEmitter<void> = new EventEmitter<void>();
 
-
-  login() {
-    this.onLogin.emit(); 
-  }
-  togglePasswordVisibility(input: any) {
-    this.showPassword = !this.showPassword;
-    input.type = this.showPassword ? 'text' : 'password';
+  login () {
+    this.onLogin.emit()
   }
 
+  togglePasswordVisibility (input: any) {
+    this.showPassword = !this.showPassword
+    input.type = this.showPassword ? 'text' : 'password'
+  }
 }
