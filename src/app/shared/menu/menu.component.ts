5f3ae1a5296b447d0bf811d8e32a5f72c0803407
Juan Demera <jpdemera@espol.edu.ec>
2023-10-28 16:20:51 -0500

    menu funcional 95%

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -11,4 +11,20 @@ export class MenuComponent {
   logout(){
     this.onLogout.emit();
   }
+
+  selectedOption: string = ''; // Inicialmente ninguna opción seleccionada
+
+  // Función para cambiar la opción seleccionada
+  changeSelectedOption(option: string) {
+    if (this.selectedOption !== option) {
+      this.selectedOption = option; // Establecer la nueva opción
+    }
+   /* if (this.selectedOption === option) {
+      
+      this.selectedOption = ''; // Restablecer la opción si ya está seleccionada
+    } else {
+      this.selectedOption = option; // Establecer la nueva opción
+    }*/
+  }
+  
 }
